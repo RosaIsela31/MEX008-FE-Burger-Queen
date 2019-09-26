5440e4fdd600c4f6b6b59fd39da239d5e27d6568
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-09-26 14:25:09 -0500

    change rooute from pizza in file's Pozzas.jsx

--- pandmonium-app/src/views/subviews/Pizzas.jsx
+++ pandmonium-app/src/views/subviews/Pizzas.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Pizzas from '../../components/generalMenu/menus/pizzas/Pizzas';
+import Pizzas from '../../components/generalMenu/menus/pizzas/pizzas';
 import Toolbar from '../../components/sidebar/toolbar/Toolbar';
 
 class PizzasMenu extends React.Component {
