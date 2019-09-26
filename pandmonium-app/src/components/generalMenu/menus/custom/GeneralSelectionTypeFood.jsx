cfaff566088f4aba0379d3d66375c48387b86a31
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-09-25 19:45:38 -0500

    creating folder custom

---
+++ pandmonium-app/src/components/generalMenu/menus/custom/GeneralSelectionTypeFood.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import GeneralIcon from '../../food';
+// import iconFood from '';
+
+// function GeneralSelectionTypeFood (){
+//   return(
+//     <GeneralIcon icon={iconFood}/>
+//   )
+// }
+
+// export default GeneralSelectionTypeFood;
+
---
+++ pandmonium-app/src/components/generalMenu/menus/custom/GeneralViewCustom.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import GeneralBtnOrder from './GeneralBtnOrder.jsx'
+
+
+// const GeneralViewCustom =()=>{
+
+//   return(
+//     <GeneralBtnOrder/>
+
+
+//   )
+// }
+
+// export default GeneralViewCustom;
