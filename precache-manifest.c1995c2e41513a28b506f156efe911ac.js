2243859ca5676cfd7c5673233ea7dacdb5b32452
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-10-07 12:37:18 -0500

    Updates

--- precache-manifest.c1995c2e41513a28b506f156efe911ac.js
+++ precache-manifest.c1995c2e41513a28b506f156efe911ac.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "33e4f693734229462e4dd52fed09426d",
+    "revision": "942bdff2c589f4fd8f8acd2c6e996084",
     "url": "/MEX008-FE-Burger-Queen/index.html"
   },
   {
-    "revision": "b4c349f150554bf47e40",
-    "url": "/MEX008-FE-Burger-Queen/static/css/main.3ca42bbf.chunk.css"
+    "revision": "e3c089f4085951324247",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.13550ee9.chunk.css"
   },
   {
-    "revision": "0deefa4ed1a2fd240ea9",
-    "url": "/MEX008-FE-Burger-Queen/static/js/2.90147aaa.chunk.js"
+    "revision": "10e3de4a6a4b5135db6d",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.ee81bdc7.chunk.js"
   },
   {
-    "revision": "b4c349f150554bf47e40",
-    "url": "/MEX008-FE-Burger-Queen/static/js/main.27f4be01.chunk.js"
+    "revision": "e3c089f4085951324247",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.75f9f0fd.chunk.js"
   },
   {
     "revision": "d2c7ca4ba3a2048f236c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MEX008-FE-Burger-Queen/precache-manifest.31ec7a1d2c3b1cbc94f5607e4f1cde64.js"
+  "/MEX008-FE-Burger-Queen/precache-manifest.c1995c2e41513a28b506f156efe911ac.js"
 );
 
 self.addEventListener('message', (event) => {
