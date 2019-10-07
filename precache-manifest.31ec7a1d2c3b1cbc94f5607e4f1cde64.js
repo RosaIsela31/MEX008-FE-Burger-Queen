647e791192a4bd2912aa28cabcebeda744a258a2
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-10-06 22:16:08 -0500

    Updates

---
+++ precache-manifest.31ec7a1d2c3b1cbc94f5607e4f1cde64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e4f693734229462e4dd52fed09426d",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "b4c349f150554bf47e40",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.3ca42bbf.chunk.css"
+  },
+  {
+    "revision": "0deefa4ed1a2fd240ea9",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.90147aaa.chunk.js"
+  },
+  {
+    "revision": "b4c349f150554bf47e40",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.27f4be01.chunk.js"
+  },
+  {
+    "revision": "d2c7ca4ba3a2048f236c",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.c891d290.js"
+  },
+  {
+    "revision": "ba91bfda1923ab6a9f09d289bcb8154b",
+    "url": "/MEX008-FE-Burger-Queen/static/media/logo.ba91bfda.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MEX008-FE-Burger-Queen/precache-manifest.31ec7a1d2c3b1cbc94f5607e4f1cde64.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MEX008-FE-Burger-Queen/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
